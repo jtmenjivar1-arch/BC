1a626935863caa527ef4a6ae1db5083aca6eeed9
jtmenjivar1-arch <jtmenjivar1@gmail.com>
2026-05-14 18:48:11 -0600

    Add files via upload

---
+++ PAGINA WEB ACT 070426/PAGINA WEB ACT 070426/js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://gzwcdmcxpbkqnszpfwoa.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_1X-z22Y2rIcxCdEG475Gbg_GodawfdH';
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
